3fde96e2791e5c52ec7fb0aa3544291c2916f889
KanHar <knaan.harpaz@gmail.com>
2022-12-05 22:19:23 +0200

    Remove bad print

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,6 @@ const octokit = new Octokit({
   auth: process.env.GITHUB_TOKEN
 })
 
-console.log(process.env.GITHUB_TOKEN);
-
 async function run (): Promise<void> {
   try {
     // Get the pull request number and repository owner and name from the context object
