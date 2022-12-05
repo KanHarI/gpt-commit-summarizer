e98f6c8ad8d48b36f31b5aeecd6fd3182a7bd6ba
KanHar <knaan.harpaz@gmail.com>
2022-12-05 22:49:37 +0200

    Correct commit messages

--- src/index.ts
+++ src/index.ts
@@ -27,13 +27,19 @@ async function run (): Promise<void> {
 
   // For each commit, get the list of files that were modified
   for (const commit of commits.data) {
-    console.log(commit.sha)
-    console.log(commit.files)
-    // Filter the list of files to only include files modified in this commit
-    const files = commit.files?.filter(file => file.status !== 'removed') ?? []
+    // Get the commit object with the list of files that were modified
+    const commitObject = await octokit.repos.getCommit({
+      owner: repository.owner.login,
+      repo: repository.name,
+      ref: commit.sha
+    })
+
+    if (commitObject.data.files === undefined) {
+      throw new Error('Files undefined')
+    }
 
     // Create a comment on the pull request with the names of the files that were modified in the commit
-    const comment = `Files modified in commit ${commit.sha}: ${files
+    const comment = `Files modified in commit ${commit.sha}: ${commitObject.data.files
       .map((file) => file.filename)
       .join(', ')}`
     await octokit.issues.createComment({
