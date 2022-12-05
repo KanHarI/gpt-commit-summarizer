5c07aa43860d97352e6f97fe250d70d20788819a
KanHar <knaan.harpaz@gmail.com>
2022-12-05 21:08:41 +0200

    WIP

--- src/index.ts
+++ src/index.ts
@@ -4,43 +4,48 @@ import { context } from '@actions/github'
 const octokit = new Octokit()
 
 async function run (): Promise<void> {
-  // Get the pull request number and repository owner and name from the context object
-  const {
-    number,
-    repository
-  } = (context.payload.pull_request as {
-    number: number
-    repository: { owner: string, repo: string }
-  })
-  const { owner, repo } = repository
-
-  // Get the list of commits for the pull request
-  const commits = await octokit.pulls.listCommits({
-    owner,
-    repo,
-    pull_number: number
-  })
-
-  // For each commit, get the list of files that were modified
-  for (const commit of commits.data) {
-    const files = (await octokit.pulls.listFiles({
-      owner,
-      repo,
-      pull_number: number,
-      commit_sha: commit.sha
-    })).data
-
-    // Create a comment on the pull request with the names of the files that were modified in the commit
-    const comment = `Files modified in commit ${commit.sha}: ${files
-      .map((file) => file.filename)
-      .join(', ')}`
-    await octokit.issues.createComment({
-      owner,
-      repo,
-      issue_number: number,
-      body: comment
+  try {
+    // Get the pull request number and repository owner and name from the context object
+    const {
+      number,
+      repository
+    } = (context.payload.pull_request as {
+      number: number
+      repository: { owner: string, repo: string }
     })
+    const { owner, repo } = repository
+
+    // Get the list of commits for the pull request
+    const commits = await octokit.pulls.listCommits({
+      owner,
+      repo,
+      pull_number: number
+    })
+
+    // For each commit, get the list of files that were modified
+    for (const commit of commits.data) {
+      const files = (await octokit.pulls.listFiles({
+        owner,
+        repo,
+        pull_number: number,
+        commit_sha: commit.sha
+      })).data
+
+      // Create a comment on the pull request with the names of the files that were modified in the commit
+      const comment = `Files modified in commit ${commit.sha}: ${files
+        .map((file) => file.filename)
+        .join(', ')}`
+      await octokit.issues.createComment({
+        owner,
+        repo,
+        issue_number: number,
+        body: comment
+      })
+    }
+  } catch (error) {
+    // Handle any errors that may occur
+    console.error(error)
   }
 }
 
-console.log('SANITY')
+run()
