88db9e6c5d00fbfb79c2cd4df45bc2bb8f86adb5
KanHar <knaan.harpaz@gmail.com>
2022-12-06 01:49:35 +0200

    WIP

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ async function run (): Promise<void> {
   const perPage = 100
 
   // Get the list of existing comments for the pull request
-  const comments: Awaited<ReturnType<typeof octokit.issues.listComments>> = [] as unknown as Awaited<ReturnType<typeof octokit.issues.listComments>>
+  const comments: Awaited<ReturnType<typeof octokit.issues.listComments>>['data'] = []
   let page = 1
   while (true) {
     // Get the current page of comments
@@ -38,11 +38,8 @@ async function run (): Promise<void> {
       break
     }
 
-    console.log(response)
-
     // Add the comments from the current page to the list of comments
-    // @ts-expect-error
-    comments.push(...Array.from(response.data))
+    comments.push(...response.data)
 
     // If there are no more pages of comments, break out of the loop
     if ((response.headers.link?.includes('next')) === false) {
@@ -56,11 +53,35 @@ async function run (): Promise<void> {
   console.log(comments)
 
   // Get the list of diffs for the pull request
-  const diffs = await octokit.pulls.listFiles({
-    owner: repository.owner.login,
-    repo: repository.name,
-    pull_number: number
-  })
+  const diffs: Awaited<ReturnType<typeof octokit.pulls.listFiles>>['data'] = []
+  page = 1
+  while (true) {
+    // Get the current page of diffs
+    const response = await octokit.pulls.listFiles({
+      owner: repository.owner.login,
+      repo: repository.name,
+      pull_number: number,
+      per_page: perPage,
+      page
+    })
+
+    if (response === undefined) {
+      break
+    }
+
+    // Add the diffs from the current page to the list of diffs
+    diffs.push(...response.data)
+
+    // If there are no more pages of diffs, break out of the loop
+    if ((response.headers.link?.includes('next')) === false) {
+      break
+    }
+
+    // Increment the page number
+    page++
+  }
+
+  console.log(diffs)
 
   // For each commit, get the list of files that were modified
   const commits = await octokit.pulls.listCommits({
@@ -73,7 +94,7 @@ async function run (): Promise<void> {
     // Check if a comment for this commit already exists
     const expectedComment = `GPT summary of ${commit.sha}: `
     const regex = new RegExp(`^${expectedComment}.*$`)
-    const existingComment = (comments as unknown as typeof comments.data).find((comment) => regex.test(comment.body ?? ''))
+    const existingComment = (comments).find((comment) => regex.test(comment.body ?? ''))
 
     // If a comment already exists, skip this commit
     if (existingComment !== undefined) {
@@ -92,8 +113,7 @@ async function run (): Promise<void> {
     }
 
     // Find the first diff that corresponds to one of the modified files in the commit
-    // Find the first diff that corresponds to one of the modified files in the commit
-    const diff = diffs.data.find((file) => commitObject.data.files?.some((commitFile) => commitFile.filename === file.filename))
+    const diff = diffs.find((file) => commitObject.data.files?.some((commitFile) => commitFile.filename === file.filename))
 
     // If no diff is found, skip this commit
     if (diff === undefined) {
