171b47dc93d0b9b2ea505f19213fec6c60460879
KanHar <knaan.harpaz@gmail.com>
2022-12-08 23:21:55 +0200

    Gather diff urls

--- src/filesSummary.ts
+++ src/filesSummary.ts
@@ -1,16 +1,21 @@
 import { octokit } from './octokit'
 
 // Get list of files changed between the base and the end of the pull request
-export async function getFilesSummaries (issueNumber: number,
-  repository: { owner: { login: string }, name: string }): Promise<Record<string, string>> {
+export async function getFilesSummaries (pullNumber: number,
+  repository: { owner: { login: string }, name: string }): Promise<Record<string, [string, string]>> {
   const filesChanged = await octokit.pulls.listFiles({
     owner: repository.owner.login,
     repo: repository.name,
-    pull_number: issueNumber
+    pull_number: pullNumber
   })
-  const modifiedFiles: Record<string, string> = {}
-  filesChanged.data.forEach((file: any) => {
-    modifiedFiles[file.filename] = file.sha
+  const modifiedFiles: Record<string, [string, string]> = {}
+  const diff = await octokit.pulls.get({
+    owner: repository.owner.login,
+    repo: repository.name,
+    pull_number: pullNumber
   })
+  for (const file of filesChanged.data) {
+    modifiedFiles[file.filename] = [file.sha, diff.data.diff_url]
+  }
   return modifiedFiles
 }
