734cc084b5f104abf26d6e33e4686fce9b6aebe4
KanHar <knaan.harpaz@gmail.com>
2022-12-08 13:50:51 +0200

    Refactor the commit summarizer

---
+++ src/DiffMetadata.ts
@@ -0,0 +1,9 @@
+import {PayloadRepository} from "@actions/github/lib/interfaces";
+import {octokit} from "./octokit";
+
+export interface gitDiffMetadata {
+  sha: string
+  issueNumber: number
+  repository: PayloadRepository
+  commit: Awaited<ReturnType<typeof octokit.repos.getCommit>>
+}
