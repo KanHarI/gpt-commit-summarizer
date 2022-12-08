410df74327191224cdcaf6414e1bc0f5982da54a
KanHar <knaan.harpaz@gmail.com>
2022-12-08 23:51:50 +0200

    Add pagination

--- src/filesSummary.ts
+++ src/filesSummary.ts
@@ -1,8 +1,8 @@
 import { octokit } from './octokit'
 import { PayloadRepository } from '@actions/github/lib/interfaces'
 
-async function getReviewComment (pullRequestNumber: number, repository: PayloadRepository): Promise<void> {
-  const reviewComments = await octokit.pulls.listReviewComments({
+async function getReviewComments (pullRequestNumber: number, repository: PayloadRepository): Promise<void> {
+  const reviewComments = await octokit.paginate(octokit.pulls.listReviewComments, {
     owner: repository.owner.login,
     repo: repository.name,
     pull_number: pullRequestNumber
@@ -21,6 +21,6 @@ export async function getFilesSummaries (pullNumber: number,
   for (const file of filesChanged.data) {
     modifiedFiles[file.filename] = [file.sha, file.patch ?? '']
   }
-  await getReviewComment(pullNumber, repository)
+  await getReviewComments(pullNumber, repository)
   return modifiedFiles
 }
