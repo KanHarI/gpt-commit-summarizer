2522f6092c359fa2bbef3cc4d4d5b47f2b9baa0b
KanHar <knaan.harpaz@gmail.com>
2022-12-09 20:12:18 +0200

    Update position to add comment on

--- src/filesSummary.ts
+++ src/filesSummary.ts
@@ -185,7 +185,7 @@ export async function getFilesSummaries(
       pull_number: pullNumber,
       commit_id: headCommitSha,
       path: modifiedFiles[modifiedFile].filename,
-      line: modifiedFiles[modifiedFile].position,
+      line: modifiedFiles[modifiedFile].position + 1,
       body: comment,
     });
     summarizedFiles += 1;
